9c15580af9ba130f8984d915ff38fd64925f8a9e
pekellogg <pekellogg@hotmail.com>
2021-03-19 13:16:12 -0700

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -1,9 +1,23 @@
+# displays Tic Tac Toe board
 def display_board(board)
+  puts
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  puts
 end
 
-# code your input_to_index and move method here!
+# convert user input from str to int, return int as index
+def input_to_index(user_input)
+  index = user_input.to_i - 1
+  return index
+end
+
+# updates values on board, calls display_board
+def move(board, index, player = "X")
+  board[index] = player
+  
+  # display_board(board)
+end
